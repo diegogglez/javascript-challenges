0cfc51c9a31e5f4ea60fcda35e9c4dc600c34d92
diegogglez <diegomiller7@gmail.com>
2023-01-26 16:30:24 +0100

    refactor: cleaning the code, leave the reduce method only

--- digitalRoot.js
+++ digitalRoot.js
@@ -1,19 +1,3 @@
-// let digitalRoot = (n) => {
-//   result = 0;
-//   for (let i = 0; i < n.toString().length; i++) {
-//     result += Math.floor(n.toString()[i]);
-//   }
-//   return result;
-// }
-
-// console.log(digitalRoot(195));
-
-// let digitalRoot2 = (n) => {
-//   let arr = n.toString().split('');
-// }
-
-// console.log(digitalRoot2(123));
-
 const digitalRoot3 = (number) => {
   const numArr = number.toString().split("");
   return numArr.reduce((acc, value) => {

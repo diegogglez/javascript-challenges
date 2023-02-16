bccbfb7dab50324f2c24834936697ed077acca3c
diegogglez <diegomiller7@gmail.com>
2023-02-16 23:37:37 +0100

    Refactor: adding an alternative solution with replace method

--- camelCaseSplit.js
+++ camelCaseSplit.js
@@ -5,6 +5,8 @@
 
 const isUpperCase = (letter) => letter === letter.toUpperCase();
 
+//* Solution 1:
+
 const camelCaseSpliter = (str) => {
   if (str.split('').some(isUpperCase)) {
     const strArr = str.split('');
@@ -16,14 +18,26 @@ const camelCaseSpliter = (str) => {
         : strSplited += letter;
     }
 
-    return strSplited;
+    return strSplited.trim();
 
-  } else return str;
+  } else return str.trim();
 } 
-
 
 console.log(camelCaseSpliter('camelCasing'));
 console.log(camelCaseSpliter('otherDoFirstSeem'));
 console.log(camelCaseSpliter('helloFellas'));
 console.log(camelCaseSpliter('hello'));
+console.log(camelCaseSpliter('Hello'));
+
+//* Solution 2:
+
+const camelCaseSpliter2 = (string) => string.replace(/([A-Z])/g, ' $1').trim();
+
+console.log(camelCaseSpliter2('camelCasing'));
+console.log(camelCaseSpliter2('otherDoFirstSeem'));
+console.log(camelCaseSpliter2('helloFellas'));
+console.log(camelCaseSpliter2('hello'));
+console.log(camelCaseSpliter2('Hello'));
+
+
 

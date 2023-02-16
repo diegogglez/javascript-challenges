b71e2d055c45bd74271bc9794745e7f6a92cd739
diegogglez <diegomiller7@gmail.com>
2023-02-16 23:55:49 +0100

    Refactor: playing around with more solutions

--- camelCaseSplit.js
+++ camelCaseSplit.js
@@ -5,7 +5,7 @@
 
 const isUpperCase = (letter) => letter === letter.toUpperCase();
 
-//* Solution 1:
+//* Solution 1 (iteration loop):
 
 const camelCaseSpliter = (str) => {
   if (str.split('').some(isUpperCase)) {
@@ -29,7 +29,7 @@ console.log(camelCaseSpliter('helloFellas'));
 console.log(camelCaseSpliter('hello'));
 console.log(camelCaseSpliter('Hello'));
 
-//* Solution 2:
+//* Solution 2 (regular expressions and replace method):
 
 const camelCaseSpliter2 = (string) => string.replace(/([A-Z])/g, ' $1').trim();
 
@@ -38,6 +38,26 @@ console.log(camelCaseSpliter2('otherDoFirstSeem'));
 console.log(camelCaseSpliter2('helloFellas'));
 console.log(camelCaseSpliter2('hello'));
 console.log(camelCaseSpliter2('Hello'));
+
+
+//* Solution 3 (spread operator and map method):
+
+// let stringTest = 'hola';
+// let stringTest2 = [...stringTest];
+
+// console.log(stringTest);
+// console.log(stringTest2);
+
+const camelCaseSpliter3 = string => {
+  return [...string].map(
+    (char) => char === char.toUpperCase() ? ` ${char}` : char
+    ).join('');
+}
+
+console.log(camelCaseSpliter3('hola'));
+console.log(camelCaseSpliter3('holaBuenas'));
+
+
 
 
 
